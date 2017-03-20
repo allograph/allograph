98dd80a85641d058e9437091cea7d9d4231c03c6
Ting Chou <tina77928@gmail.com>
2017-03-20 11:54:59 -0500

    add one-many relations

--- db_data.js
+++ db_data.js
@@ -5,8 +5,8 @@ var connection = new pg.Client({
   user: 'rachelminto',
   database: 'relay',
   password: 'postgres',
-  host: 'localhost', 
-  port: 5432, 
+  host: 'localhost',
+  port: 5432,
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 3000, // how long a client is allowed to remain idle
 });
@@ -15,7 +15,7 @@ var DBData = function () {};
 connection.connect();
 
 DBData.prototype.readSchema = function (callback) {
-  Promise.all([tableNamesQuery, dbNameQuery]).then(values => { 
+  Promise.all([tableNamesQuery, dbNameQuery]).then(values => {
     var dbName = values[1].rows[0].catalog_name
     var tables = values[0].rows
 
@@ -45,10 +45,11 @@ DBData.prototype.readSchema = function (callback) {
         meta.tables[tableName] = table
       });
 
+      writeRelation(meta, relations);
       writeToJSON(meta);
       callback(meta);
     });
-  });  
+  });
 };
 
 var writeToJSON = function(meta) {
@@ -57,11 +58,11 @@ var writeToJSON = function(meta) {
   });
 }
 
-var tableNamesQuery = connection.query(`SELECT * FROM information_schema.tables 
+var tableNamesQuery = connection.query(`SELECT * FROM information_schema.tables
   WHERE table_schema = 'public'`)
 
 function tableInfo(tableName) {
-  return connection.query(`SELECT column_name, data_type, is_nullable, table_name, 
+  return connection.query(`SELECT column_name, data_type, is_nullable, table_name,
     column_default FROM information_schema.columns WHERE table_name = $1`, [tableName])
 }
 
@@ -75,7 +76,7 @@ function functionsFor(tables) {
   return tableFunctions
 }
 
-var dbNameQuery = connection.query(`SELECT * FROM 
+var dbNameQuery = connection.query(`SELECT * FROM
   information_schema.information_schema_catalog_name;`)
 
 var columnData = function(field) {
@@ -86,6 +87,62 @@ var columnData = function(field) {
   }
 
   return data;
+};
+
+var relationQuery = connection.query(`
+SELECT
+  fk.table_name AS FK_Table,
+  cu.column_name AS FK_Column,
+  fk.constraint_type AS fk_constraint_type,
+  pk.table_name AS PK_Table,
+  pt.column_name AS PK_Column,
+  pk.constraint_type AS pk_constraint_type,
+  c.constraint_name,
+  c.update_rule,
+  c.delete_rule
+FROM
+  information_schema.referential_constraints AS c
+INNER JOIN
+  information_schema.table_constraints AS fk
+  ON c.constraint_name = fk.constraint_name
+INNER JOIN
+  information_schema.table_constraints AS pk
+  ON c.unique_constraint_name = pk.constraint_name
+INNER JOIN
+  information_schema.key_column_usage AS cu
+  ON c.constraint_name = cu.constraint_name
+INNER JOIN
+  (
+    SELECT
+      i1.table_name,
+      i2.column_name
+    FROM
+      information_schema.table_constraints AS i1
+    INNER JOIN
+      information_schema.key_column_usage AS i2
+    ON
+      i1.constraint_name = i2.constraint_name
+    WHERE i1.constraint_type = 'PRIMARY KEY'
+  ) AS pt
+  ON
+    pt.table_name = pk.table_name
+`);
+
+var writeRelation = function(meta, relations) {
+  relations.forEach(function(relation) {
+    meta.tables[relation["pk_table"]].fields[relation["fk_table"]] = {
+      data_type: "list"
+    }
+
+    delete meta.tables[relation["fk_table"]].fields[relation["fk_column"]]
+    meta.tables[relation["fk_table"]].fields[relation["pk_table"]] = {
+      data_type: "object",
+      update_rule: relation["update_rule"],
+      delete_rule: relation["delete_rule"]
+    }
+  });
+
+  return meta;
 };
 
 exports.DBData = new DBData();
